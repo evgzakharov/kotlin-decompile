9766e3d34dd5032628c4fe9e16da0a1284c1ff6a
evgzakharov <evgzakharov88@gmail.com>
2019-02-25 19:15:37 +0300

    remove sequence

--- src/main/kotlin/examples/demo/Collection.kt
+++ src/main/kotlin/examples/demo/Collection.kt
@@ -3,7 +3,7 @@ package examples.demo
 fun kollectionTest() {
     val collectionTest = listOf("1")
 
-    val resultMap = collectionTest.asSequence()
+    val resultMap = collectionTest
         .filter { it.length > 1 }
         .map { it.toString() }
         .map { it.toString() + "__" }
